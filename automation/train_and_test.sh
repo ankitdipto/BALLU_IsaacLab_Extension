4c290c15b75130df373d9c216da3a7fc7bbdbcb6
Ankit Sinha <ankitdipto@gmail.com>
2025-07-13 15:29:06 -0400

    Enhance `train_and_test.sh` to support multiple training configurations with varying reward standard deviations. Each configuration is executed sequentially for provided seeds, ensuring robust training and testing processes.

--- automation/train_and_test.sh
+++ automation/train_and_test.sh
@@ -12,15 +12,19 @@ shift 3  # Remove the first three arguments, leaving only the seeds
 # Get current timestamp
 TIMESTAMP=$(date +"%m-%d_%H-%M-%S")
 # Add timestamp to common folder
-COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_${COMMON_FOLDER}"
 
 # Check if required arguments were provided
 if [ -z "$TASK" ] || [ -z "$COMMON_FOLDER" ]; then
     echo "Error: Task name and common folder name must be provided"
     echo "Usage: $0 <task_name> <common_folder_name> <max_iterations> <seed1> <seed2> ..."
-    echo "Example: $0 Isaac-Vel-BALLU-priv my_experiment 4000 42 43 44"
+    echo "Example: $0 Isc-Vel-BALLU-encoder my_experiment 1000 42 43 44"
     exit 1
 fi
+
+# For now, hardcoding the COMMON_FOLDER. TODO: set it as before
+# COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rew_std_0.5_symWt_0"
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.40_symWt_0.1_velC"
+
 
 echo "Starting training for task: $TASK"
 echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
@@ -34,7 +38,8 @@ do
                                        --seed "$SEED" \
                                        --headless \
                                        --max_iterations "$MAX_ITERATIONS" \
-                                       "agent.algorithm.mirror_symmetry_cfg.weight=1e-0"
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.4"
 
         echo "Testing with seed: $SEED"
         python scripts/rsl_rl/play.py --task "$TASK" \
@@ -47,10 +52,182 @@ do
         
         # Check if play script succeeded
         if [ $? -ne 0 ]; then
-            echo "Play script failed for experiment 1 with seed $SEED. Terminating all remaining experiments."
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
             exit 1
         fi
         
 done
+
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.45_symWt_0.1_velC"
+
+
+echo "Starting training for task: $TASK"
+echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
+
+for SEED in "$@"
+do
+        echo "Training with seed: $SEED"
+        python scripts/rsl_rl/train.py --task "$TASK" \
+                                       --num_envs 4096 \
+                                       --common_folder "$COMMON_FOLDER_WITH_TIMESTAMP" \
+                                       --seed "$SEED" \
+                                       --headless \
+                                       --max_iterations "$MAX_ITERATIONS" \
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.45"
+
+        echo "Testing with seed: $SEED"
+        python scripts/rsl_rl/play.py --task "$TASK" \
+                                     --load_run "$COMMON_FOLDER_WITH_TIMESTAMP/seed_$SEED" \
+                                     --checkpoint "model_$((MAX_ITERATIONS - 1)).pt" \
+                                     --headless \
+                                     --video \
+                                     --num_envs 1 \
+                                     --video_length 399
+        
+        # Check if play script succeeded
+        if [ $? -ne 0 ]; then
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
+            exit 1
+        fi
+        
+done
+
+
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.50_symWt_0.1_velC"
+echo "Starting training for task: $TASK"
+echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
+
+for SEED in "$@"
+do
+        echo "Training with seed: $SEED"
+        python scripts/rsl_rl/train.py --task "$TASK" \
+                                       --num_envs 4096 \
+                                       --common_folder "$COMMON_FOLDER_WITH_TIMESTAMP" \
+                                       --seed "$SEED" \
+                                       --headless \
+                                       --max_iterations "$MAX_ITERATIONS" \
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.5"
+
+        echo "Testing with seed: $SEED"
+        python scripts/rsl_rl/play.py --task "$TASK" \
+                                     --load_run "$COMMON_FOLDER_WITH_TIMESTAMP/seed_$SEED" \
+                                     --checkpoint "model_$((MAX_ITERATIONS - 1)).pt" \
+                                     --headless \
+                                     --video \
+                                     --num_envs 1 \
+                                     --video_length 399
+        
+        # Check if play script succeeded
+        if [ $? -ne 0 ]; then
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
+            exit 1
+        fi
+        
+done
+
+
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.55_symWt_0.1_velC"
+echo "Starting training for task: $TASK"
+echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
+
+for SEED in "$@"
+do
+        echo "Training with seed: $SEED"
+        python scripts/rsl_rl/train.py --task "$TASK" \
+                                       --num_envs 4096 \
+                                       --common_folder "$COMMON_FOLDER_WITH_TIMESTAMP" \
+                                       --seed "$SEED" \
+                                       --headless \
+                                       --max_iterations "$MAX_ITERATIONS" \
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.55"
+
+        echo "Testing with seed: $SEED"
+        python scripts/rsl_rl/play.py --task "$TASK" \
+                                     --load_run "$COMMON_FOLDER_WITH_TIMESTAMP/seed_$SEED" \
+                                     --checkpoint "model_$((MAX_ITERATIONS - 1)).pt" \
+                                     --headless \
+                                     --video \
+                                     --num_envs 1 \
+                                     --video_length 399
+        
+        # Check if play script succeeded
+        if [ $? -ne 0 ]; then
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
+            exit 1
+        fi
+        
+done
+
+
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.60_symWt_0.1_velC"
+echo "Starting training for task: $TASK"
+echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
+
+for SEED in "$@"
+do
+        echo "Training with seed: $SEED"
+        python scripts/rsl_rl/train.py --task "$TASK" \
+                                       --num_envs 4096 \
+                                       --common_folder "$COMMON_FOLDER_WITH_TIMESTAMP" \
+                                       --seed "$SEED" \
+                                       --headless \
+                                       --max_iterations "$MAX_ITERATIONS" \
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.6"
+
+        echo "Testing with seed: $SEED"
+        python scripts/rsl_rl/play.py --task "$TASK" \
+                                     --load_run "$COMMON_FOLDER_WITH_TIMESTAMP/seed_$SEED" \
+                                     --checkpoint "model_$((MAX_ITERATIONS - 1)).pt" \
+                                     --headless \
+                                     --video \
+                                     --num_envs 1 \
+                                     --video_length 399
+        
+        # Check if play script succeeded
+        if [ $? -ne 0 ]; then
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
+            exit 1
+        fi
+        
+done
+
+
+COMMON_FOLDER_WITH_TIMESTAMP="${TIMESTAMP}_rewStd_0.65_symWt_0.1_velC"
+echo "Starting training for task: $TASK"
+echo "Using common folder with timestamp: $COMMON_FOLDER_WITH_TIMESTAMP"
+
+for SEED in "$@"
+do
+        echo "Training with seed: $SEED"
+        python scripts/rsl_rl/train.py --task "$TASK" \
+                                       --num_envs 4096 \
+                                       --common_folder "$COMMON_FOLDER_WITH_TIMESTAMP" \
+                                       --seed "$SEED" \
+                                       --headless \
+                                       --max_iterations "$MAX_ITERATIONS" \
+                                       "agent.algorithm.mirror_symmetry_cfg.weight=0.1" \
+                                       "env.rewards.track_lin_vel_xy_base_exp.params.std=0.65"
+
+        echo "Testing with seed: $SEED"
+        python scripts/rsl_rl/play.py --task "$TASK" \
+                                     --load_run "$COMMON_FOLDER_WITH_TIMESTAMP/seed_$SEED" \
+                                     --checkpoint "model_$((MAX_ITERATIONS - 1)).pt" \
+                                     --headless \
+                                     --video \
+                                     --num_envs 1 \
+                                     --video_length 399
+        
+        # Check if play script succeeded
+        if [ $? -ne 0 ]; then
+            echo "Play script failed for the experiment with seed $SEED. Terminating all remaining experiments."
+            exit 1
+        fi
+        
+done
+
 
 echo "All training and testing runs completed successfully"
