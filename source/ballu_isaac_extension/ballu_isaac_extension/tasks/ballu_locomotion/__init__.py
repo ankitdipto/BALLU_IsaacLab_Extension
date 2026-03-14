94845737641be994769fac5e538c4ef952f79b5d
Ankit Sinha <ankitdipto@gmail.com>
2026-03-14 19:47:22 -0400

    Add heterogeneous environment for the ramp locomotion task where the policy is conditioned on robot morphology

--- source/ballu_isaac_extension/ballu_isaac_extension/tasks/ballu_locomotion/__init__.py
+++ source/ballu_isaac_extension/ballu_isaac_extension/tasks/ballu_locomotion/__init__.py
@@ -217,3 +217,13 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BALLUPPORunnerCfg"
     }
 )
+
+gym.register(
+    id="Isc-BALLU-hetero-general-ramp",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.ramp_hetero_general_env_cfg:BalluSingleRampHeteroEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BALLUPPORunnerCfg"
+    }
+)
